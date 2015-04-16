89f6aed383136d124766782f8f3f0a561dd20e12
Neko Code <nekocode@Neko-MacBook-Air.local>
2015-04-16 23:53:53 +0300

    Initial Commit

---
+++ kot_ton/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  kot_ton
+//
+//  Created by Neko Code on 4/16/15.
+//  Copyright (c) 2015 nekocode. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
