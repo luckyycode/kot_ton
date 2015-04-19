5d36b7ae07cba78be3f2ca946cfd3acc68cbfbc3
Neko Code <nekocode@Neko-MacBook-Air.local>
2015-04-19 23:32:38 +0300

    update

--- kot_ton/main.cpp
+++ kot_ton/main.cpp
@@ -6,7 +6,6 @@
 //  Copyright (c) 2015 nekocode. All rights reserved.
 //
 
-#include <stdlib.h>
 #include <iostream> // wcout, locale
 #include <fstream>  // wofstream ( wide )
 #include <string>
@@ -17,19 +16,30 @@
 #include <locale>   // ru_ru
 #include <wchar.h>  // wchar_t for wide char type
 
-// wchar_t - utf-8s
-const wchar_t EN_FAL = L'a';    // English.
-const wchar_t EN_LAL = L'z';
 
-const wchar_t RU_FAL = L'а';    // Russian.
-const wchar_t RU_LAL = L'я';
+// wchar_t - utf-8s
+/*static*/ const wchar_t EN_FAL = L'a';    // English.
+/*static*/ const wchar_t EN_LAL = L'z';
+
+/*static*/ const wchar_t RU_FAL = L'а';    // Russian.
+/*static*/ const wchar_t RU_LAL = L'я';
 
 // Dictionary file names.
-const char * DEF_DICT_RUSSIAN = "ru_dict.txt";
-const char * DEF_DICT_ENGLISH = "en_dict.txt";
+/*static*/ const char * DEF_DICT_RUSSIAN = "ru_dict.txt";
+/*static*/ const char * DEF_DICT_ENGLISH = "en_dict.txt";
 
-const char * DEF_SRC_RUSSIAN = "ru_source.txt";
-const char * DEF_SRC_ENGLISH = "en_source.txt";
+/*static*/ const char * DEF_SRC_RUSSIAN = "ru_source.txt";
+/*static*/ const char * DEF_SRC_ENGLISH = "en_source.txt";
+
+/*static*/ const char * LOC_CHOOSE_DICT = "Введите путь и имя файла который содержит словарь: ";
+/*static*/ const char * LOC_CHOOSE_WORDS = "Введите путь и имя файла который содержит исходное и конечное слово: ";
+/*static*/ const char * LOC_CHOOSE_LANGUAGE = "Русский(0) или Английский(1)?";
+/*static*/ const char * LOC_AVAILABLE_WORDS = "Words found in the dictionary:";
+
+/*static*/ const char * LOCALIZATION = "ru_RU.UTF-8";
+
+/*static*/ const char * LOC_LANGUAGE_RUSSIAN = "Язык: Русский.";
+/*static*/ const char * LOC_LANGUAGE_ENGLISH = "Language: English.";
 
 //
 //  Error.
@@ -40,10 +50,6 @@ void com_error( const char * err );
 void com_pause();
 
 //
-//  Unicode char to UTF8 string.
-const wchar_t * unicode_to_utf8( wchar_t c );
-
-//
 //  String to wide string.
 std::wstring to_wstr( std::string & s );
 
@@ -51,7 +57,7 @@ int main( int argc, char ** argv ) {
 
     //
     // Set locale.
-    std::locale rus( "ru_RU.UTF-8" ); // Independ ( rus/eng ).
+    std::locale rus( LOCALIZATION ); // Independ ( rus/eng ).
     std::wcout.imbue( rus ); // Wide cout locale.
     
     //const char *funnything = "а А";
@@ -72,7 +78,7 @@ int main( int argc, char ** argv ) {
     std::unordered_set<std::wstring> dict;
     std::queue<std::wstring> word_data;
     std::map<std::wstring, bool> last_seen;
-    
+
     std::wstring last_str, prev_str, start, end;
     std::string word_file, dict_file;
     
@@ -81,25 +87,25 @@ int main( int argc, char ** argv ) {
     
     //
     // Language chooser.
-    std::cout << "Русский(0) или Английский(1)?" << "\n";
+    std::cout << LOC_CHOOSE_LANGUAGE << "\n";
     fscanf( stdin, "%i", &is_eng );
     if( is_eng == 0 ) {
-        std::cout << "Язык: Русский." << "\n";
+        std::cout << LOC_LANGUAGE_RUSSIAN << "\n";
         preferred_fal = RU_FAL;
         preferred_lal = RU_LAL;
     }
     else {
-        std::cout << "Language: English." << "\n";
+        std::cout << LOC_LANGUAGE_ENGLISH << "\n";
         preferred_fal = EN_FAL;
         preferred_lal = EN_LAL;
     }
     
-    std::cout << "Введите путь и имя файла который содержит исходное и конечное слово: " << "\n";
+    std::cout << LOC_CHOOSE_WORDS << "\n";
     std::cin >> word_file;
-    std::cout << "Введите путь и имя файла который содержит словарь: " << "\n";
+    std::cout << LOC_CHOOSE_DICT << "\n";
     std::cin >> dict_file;
     
-    dict.clear();
+    //dict.clear();
     
     //
     // Words.
@@ -121,26 +127,42 @@ int main( int argc, char ** argv ) {
     }
     
     //
-    // UTF-8 text data.
+    // Set UTF-8 text data.
     wide_stream_words.imbue( rus );
     wide_stream_dict.imbue( rus );
     
     //
     // Read data.
     std::wstring wbuff;
+    
+    std::cout << LOC_AVAILABLE_WORDS << "\n";
     while( getline( wide_stream_dict, wbuff ) ) { // td: free
         dict.insert( wbuff );
+        
         std::wcout << wbuff << "\n";
+    }
+    
+    if( dict.size() <= 0 ) {
+        com_error( "No words found in the dictionary." );
+        return -1;
     }
     
     wide_stream_dict.close();
     
-    getline( wide_stream_words, start );
-    getline( wide_stream_words, end );
+    // td: portable
+    if( !getline( wide_stream_words, start ) ) { // td: free
+        com_error( "Couldn't get begin word from source file.." );
+        return -1;
+    }
+    
+    if( !getline( wide_stream_words, end ) ) { // td: free
+        com_error( "Couldn't get end word from source file..\n" );
+        return -1;
+    }
     
     wide_stream_words.close();
     
-    if( start.size() != end.size() ) {
+    if( start.size() != end.size() ) { // strlen
         com_error( "Begin and destination words must be the same length.\n" );
         return -1;
     }
@@ -155,7 +177,7 @@ int main( int argc, char ** argv ) {
     std::wcout << "\nBegin: " << start << "\tDestination: " << end << "\n"; //wprintf( L"Begin: %ls\tDestination: %ls\n", start.c_str(), end.c_str() );
     printf( "- --\n" );
     
-    do {
+    do { // Do not finish because we need last word.
         while( total_words > 0 ) {
             
             --total_words;
@@ -169,7 +191,7 @@ int main( int argc, char ** argv ) {
             for( int i = 0; i < last_str.size(); ++i ) {
             //for( wchar_t i : last_str ) {
                 prev = last_str[i];
- 
+                // wchar_t
                 for( auto o = preferred_fal; o <= preferred_lal; ++o ) {
                     
                     //wcout << o << " | " << prev << endl;
@@ -180,10 +202,12 @@ int main( int argc, char ** argv ) {
                     
                     last_str[i] = o;
 
+                    // Our string is a last string.
                     if( last_str == end ) {
 
-                        std::wcout << prev_str << "\n"; //wprintf( L"%ls\n", last_str.c_str() );
-                        std::wcout << end << "\n\n\n"; //printf( "- %ls \n", end.c_str() );
+                        std::wcout << prev_str << "\n";
+                        std::wcout << end << "\n\n\n";
+                        
                         printf( "TOTAL CHARACTER CHANGES: %i\n", ar_size + 1 );
                         
                         com_pause();
@@ -191,6 +215,7 @@ int main( int argc, char ** argv ) {
                         return ar_size + 1;
                     }
                     
+                    // Skip found words.
                     if( dict.find( last_str ) != dict.end() && last_seen.find( last_str ) == last_seen.end() ) {
                         word_data.push( last_str );
                         last_seen[last_str] = true;
@@ -240,13 +265,4 @@ void com_pause() {
 #endif
     
     printf( "\n" );
-}
-
-/**
- *  Temp.
- */
-std::wstring to_wstr( std::string & s ) {
-    std::wstring temp( s.length(), L' ' );
-    std::copy( s.begin(), s.end(), temp.begin() );
-    return temp;
 }
